aa94b729ed86b37144b9394955069c0b5b55dada
salatielosorno <salatiel.linares@gmail.com>
2022-11-27 12:10:01 -0600

    chore(code): run prettier to format codebase

--- src/index.ts
+++ src/index.ts
@@ -24,9 +24,9 @@ export function _getPointsToCalculateControlsPoints({ data, xPos }: any) {
       point = data[i];
 
       next = data[Math.min(i + 1, data.length - 1) % data.length];
-      return { prev, point, next }
+      return { prev, point, next };
     }
-  };
+  }
 
   const prevIndex = nextItemIndex - 2;
   const prevItemIndex = prevIndex >= 0 ? prevIndex : 0;
@@ -36,11 +36,15 @@ export function _getPointsToCalculateControlsPoints({ data, xPos }: any) {
   point = data[currentItemIndex];
   next = data[nextItemIndex];
 
-  return { prev, point, next }
+  return { prev, point, next };
 }
 
 export function findYPositionAtX({ data, xAbsPos, tension }: any) {
-  const { prev: p0, point: p1, next: p2 } = _getPointsToCalculateControlsPoints({ data, xPos: xAbsPos })
+  const {
+    prev: p0,
+    point: p1,
+    next: p2
+  } = _getPointsToCalculateControlsPoints({ data, xPos: xAbsPos });
 
   if (p1 && p2) {
     const xDistance = p2.x - p1.x;
@@ -48,13 +52,18 @@ export function findYPositionAtX({ data, xAbsPos, tension }: any) {
       xAbsPos === data[0].x
         ? 0
         : xAbsPos === data[data.length - 1].x
-          ? 1
-          : (xAbsPos - p1.x) / xDistance; /* We need to calculate the t which have values from 0 to 1 */
+        ? 1
+        : (xAbsPos - p1.x) /
+          xDistance; /* We need to calculate the t which have values from 0 to 1 */
     const controlPoints = splineCurve(p0, p1, p2, tension);
     const { previous: PCL, next: PCR } = controlPoints;
 
     const p3 = data[data.length - 1];
-    const {prev: pi1, point: pi2, next: pi3} = _getPointsToCalculateControlsPoints({data,xPos: p3.x});
+    const {
+      prev: pi1,
+      point: pi2,
+      next: pi3
+    } = _getPointsToCalculateControlsPoints({ data, xPos: p3.x });
 
     const controlPoints2 = splineCurve(pi1, pi2, pi3, tension);
     const { previous: PCL2, next: PCR2 } = controlPoints2;
